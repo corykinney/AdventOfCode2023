bbe1fc8ca05c013aaae1e745c786f373498150a7
Cory Kinney <27692121+corykinney@users.noreply.github.com>
2023-12-06 12:37:01 -0500

    Day 6 Part 2

--- 6/boat_race.py
+++ 6/boat_race.py
@@ -3,13 +3,22 @@ import numpy as np
 
 pattern = r"\s(\d+)(?=\s)"
 with open("input.txt", "r") as f:
-    times = [int(num) for num in re.findall(pattern, f.readline())]
-    distances = [int(num) for num in re.findall(pattern, f.readline())]
+    times = re.findall(pattern, f.readline())
+    distances = re.findall(pattern, f.readline())
 
+# Part 1
 ways = []
 for t, d in zip(times, distances):
-    x = np.arange(t + 1)
-    y = x * (t - x)
-    ways.append(np.count_nonzero(y > d))
+    x = np.arange(int(t) + 1)
+    y = x * (int(t) - x)
+    ways.append(np.count_nonzero(y > int(d)))
 
 print(np.prod(ways))
+
+# Part 2
+t = int("".join(times))
+d = int("".join(distances))
+
+x1 = np.ceil((t - (t**2 - 4*d)**0.5) / 2)
+x2 = np.floor((t + (t**2 - 4*d)**0.5) / 2)
+print(int(x2 - x1 + 1))
