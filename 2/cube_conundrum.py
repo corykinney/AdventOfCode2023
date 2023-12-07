a4705c4f0d06307a8c101501dfce216e57000092
Cory Kinney <27692121+corykinney@users.noreply.github.com>
2023-12-06 21:42:48 -0500

    Day 2 Part 2

--- 2/cube_conundrum.py
+++ 2/cube_conundrum.py
@@ -3,12 +3,17 @@ import re
 red, green, blue = 12, 13, 14
 
 games = []
+powers = []
 with open("input.txt", "r") as f:
     for i, line in enumerate(f.readlines()):
         r = max([int(num) for num in re.findall(r"(\d+) red", line)])
         g = max([int(num) for num in re.findall(r"(\d+) green", line)])
         b = max([int(num) for num in re.findall(r"(\d+) blue", line)])
+
         if r <= red and g <= green and b <= blue:
             games.append(i + 1)
 
+        powers.append(r * g * b)
+
 print(sum(games))
+print(sum(powers))
